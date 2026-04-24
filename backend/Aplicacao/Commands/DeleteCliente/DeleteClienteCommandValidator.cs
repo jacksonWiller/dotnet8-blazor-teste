f314e7e5c6b9c73444718297c5f302e9e8b6df3a
jacksonWiller <jacksonwillerduarte@gmaill.com>
2026-04-23 23:06:59 -0300

    corrigir namespaces e erros de build

---
+++ backend/Aplicacao/Commands/DeleteCliente/DeleteClienteCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace Clientes.Aplicacao.Commands.DeleteCliente;
+
+public class DeleteClienteCommandValidator : AbstractValidator<DeleteClienteCommand>
+{
+    public DeleteClienteCommandValidator()
+    {
+        RuleFor(c => c.Id)
+            .NotEmpty()
+            .WithMessage("O Id do cliente é obrigatório");
+    }
+}
